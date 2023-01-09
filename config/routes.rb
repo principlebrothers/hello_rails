36279bca51475762fbfb716477975984e241719a
Ernest Anyewe Adonu <ernestadonu@yahoo.com>
2023-01-09 21:45:39 +0000

    Revert "Model, controller and route setup"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  resources :users do
-    resources :greetings
-  end
-  resources :greetings, only: %i[index show]
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
