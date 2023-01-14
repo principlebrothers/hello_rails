fcafc1638b48bd2193da50331e0d1b9dafb18b15
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-01-14 22:26:16 +0000

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative "boot"
 
 require "rails/all"
-require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -16,6 +15,7 @@ module HelloRails
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
+    config.public_file_server.enabled = true
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
