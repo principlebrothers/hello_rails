4f3303360ae9b52b4366d59894f0606f88a24488
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-01-15 10:17:23 +0000

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,19 +6,6 @@ bundle install
 bundle exec rake db:migrate
 
 # Export the development data
-RAILS_ENV=development bundle exec rake db:schema:load
+RAILS_ENV=development rake db:schema:load
 
-# Copy the exported data to the Render environment
-scp db/data.dump render@render-server:/app
-
-# SSH into the Render server
-ssh render@render-server
-
-# Go to the app directory
-cd /app
-
-# Import the data into the Render PostgreSQL database
-RAILS_ENV=production bundle exec rake db:pg_restore
-
-# Exit the SSH session
-exit
+# import the production data
