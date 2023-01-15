02aa2bc1ed2e0eebd7884cdf952ee5d351876ba9
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-01-15 10:08:49 +0000

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,3 +4,12 @@ set -o errexit
 
 bundle install
 bundle exec rake db:migrate
+
+# export the data in the development database to production
+bundle exec rake db:export
+
+# run the tests
+bundle exec rake test
+
+# run the build
+bundle exec rake build
